a5576a0c7310c0e6a35f554c88137078225dc6f0
Bright Gyamfi <bgyamfi13@gmail.com>
2025-11-23 02:24:21 -0800

    Commit Django Rest Framework

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book
 
 # Register your models here.
 
@@ -10,3 +12,14 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # columns to show
     list_filter = ('publication_year', 'author')  # filters on the right side
     search_fields = ('title', 'author')  # search bar at the top
+
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
