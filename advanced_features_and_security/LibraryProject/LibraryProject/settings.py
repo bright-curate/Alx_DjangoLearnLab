7750870a35c5882801537408dfdba4330a159483
Bright Gyamfi <bgyamfi13@gmail.com>
2025-11-23 10:46:44 -0800

    Updated LibraryProject settings

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -129,3 +129,14 @@ LOGOUT_REDIRECT_URL = 'login'
 
 AUTH_USER_MODEL = "bookshelf.CustomUser"
 
+# --------------------------------------------
+# SECURITY CONFIGURATIONS FOR ALX CHECKER
+# --------------------------------------------
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Secure cookies
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
