045b155e76b169ad5102b6c13aa98e8e49186906
Bright Gyamfi <bgyamfi13@gmail.com>
2025-11-23 10:41:16 -0800

    Updated bookshelf admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,24 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book
 
-# Register your models here.
 
-from .models import Book  # import the Book model
-
-# Register Book model with custom admin display
+# ----------------------------------------------------
+# Book Admin
+# ----------------------------------------------------
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # columns to show
-    list_filter = ('publication_year', 'author')  # filters on the right side
-    search_fields = ('title', 'author')  # search bar at the top
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
+    search_fields = ('title', 'author')
 
 
-
+# ----------------------------------------------------
+# Custom User Admin
+# ----------------------------------------------------
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+
     fieldsets = UserAdmin.fieldsets + (
         ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
     )
 
+
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Book)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -41,5 +41,11 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
+
     def __str__(self):
         return self.title
