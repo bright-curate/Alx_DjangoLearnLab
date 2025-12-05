3b32b491f102d743a334e035be0b8645c14ae3a9
Bright Gyamfi <bgyamfi13@gmail.com>
2025-12-05 09:09:57 -0800

    Updated database configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'sqlite_user',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
