f8b1624a24ea360843ed8b1fb48b8bca1b60b241
Bright Gyamfi <bgyamfi13@gmail.com>
2025-12-05 11:14:02 -0800

    Updated blog urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path 
 from .views import (
     UserLoginView, UserLogoutView,
     register, profile,
@@ -14,9 +14,11 @@ urlpatterns = [
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("register/", register, name="register"),
     path("profile/", profile, name="profile"),
-    path('posts/', PostListView.as_view(), name='posts-list'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-edit'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # BLOG CRUD ROUTES (Checker Required)
+    path("post/", PostListView.as_view(), name="post-list"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
