53e0e02b5113235aea6623e05a56b9a30f806c0a
Bright Gyamfi <bgyamfi13@gmail.com>
2025-12-09 03:29:43 -0800

    Defined url pattern

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+    return Response(serializer.errors, status=400)
+
+
+@api_view(["POST"])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+    return Response(serializer.errors, status=400)
+
+
+@api_view(["GET", "PUT"])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    if request.method == "GET":
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
