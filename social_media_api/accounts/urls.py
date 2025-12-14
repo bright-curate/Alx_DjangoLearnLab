7973346d63957c5cb9fec8433e488465984ec16e
Bright Gyamfi <bgyamfi13@gmail.com>
2025-12-14 10:44:07 -0800

    Updated social media api urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from .views import register, login, profile
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", login, name="login"),
     path("profile/", profile, name="profile"),
-    path("api/", include("accounts.urls")),
-    path("api/", include("posts.urls")),
 ]
