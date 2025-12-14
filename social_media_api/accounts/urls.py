6e9afa2d8a5620d64cd8e511b421b7613e9fcf8a
Bright Gyamfi <bgyamfi13@gmail.com>
2025-12-14 10:39:58 -0800

    Implementing posts and comments functions

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import register, login, profile
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", login, name="login"),
     path("profile/", profile, name="profile"),
+    path("api/", include("accounts.urls")),
+    path("api/", include("posts.urls")),
 ]
